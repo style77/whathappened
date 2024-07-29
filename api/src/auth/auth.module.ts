6fb30c0969227a462390c1cc814b03b5814cedde
style77 <stylek777@gmail.com>
2024-07-29 02:16:07 +0200

    chore(api): auth module

---
+++ api/src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from '../users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+    }),
+    PassportModule,
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService]
+})
+export class AuthModule { }
