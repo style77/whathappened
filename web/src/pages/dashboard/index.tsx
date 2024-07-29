4dcc444245bb839d3dbca28749be01b52234449a
style77 <stylek777@gmail.com>
2024-07-29 02:51:26 +0200

    chore(web): dashboard page

---
+++ web/src/pages/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LeftSidebar } from "../../components/dashboard/Sidebar";
+import { Layout } from "../../components/dashboard/Layout";
+
+export const Dashboard: React.FC = () => {
+  return (
+    <Layout>
+      <p>Test</p>
+    </Layout>
+  );
+};
