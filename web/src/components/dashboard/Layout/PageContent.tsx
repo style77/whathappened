f79b24a0ed24ca0f3340980826d473affa50be45
style77 <stylek777@gmail.com>
2024-07-29 15:40:33 +0200

    chore(web): set dashboard background to be a little darker

--- web/src/components/dashboard/Layout/PageContent.tsx
+++ web/src/components/dashboard/Layout/PageContent.tsx
@@ -21,7 +21,7 @@ export const PageContent: React.FC<PageContentProps> = ({ children }: PageConten
     return (
         <div className="drawer-content flex flex-col ">
             <Header />
-            <main className="flex-1 overflow-y-auto md:pt-4 pt-4 px-6 bg-gray-50" ref={mainContentRef}>
+            <main className="flex-1 overflow-y-auto md:pt-4 pt-4 px-6 bg-gray-100" ref={mainContentRef}>
                 {children}
                 <div className="h-16"></div>
             </main>
