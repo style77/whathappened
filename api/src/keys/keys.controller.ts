9a5422ddc9673cf526376f89aacf340b628a0267
style77 <stylek777@gmail.com>
2024-08-08 23:15:09 +0200

    chore: sort keys in controller

--- api/src/keys/keys.controller.ts
+++ api/src/keys/keys.controller.ts
@@ -1,16 +1,22 @@
-import { Body, Controller, Get, NotFoundException, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Patch, Post, Query, Req, UseGuards } from '@nestjs/common';
 import { KeysService } from './keys.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateKeyDto, UpdateKeyDto } from './dto/key.dto';
 
 @Controller('keys')
 export class KeysController {
-    constructor(private keysService: KeysService) {}
+    constructor(private keysService: KeysService) { }
 
     @UseGuards(JwtAuthGuard)
     @Get('/')
-    list(@Req() req) {
-        return this.keysService.findAllUserKeys(req.user);
+    list(@Req() req,
+        @Query('page') page: number = 1,
+        @Query('limit') limit: number = 10,
+        @Query('sort') sort: string = 'id',
+        @Query('order') order: 'ASC' | 'DESC' = 'ASC',
+        @Query('filter') filter?: string) {
+        const user: any = req.user;
+        return this.keysService.findAllUserKeys(user, { page, limit, sort, order, filter });
     }
 
     @UseGuards(JwtAuthGuard)
