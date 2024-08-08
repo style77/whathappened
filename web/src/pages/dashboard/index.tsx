69c126f66b0b7f8ba8fadcfb31b8a27173fea7c8
style77 <stylek777@gmail.com>
2024-08-08 23:16:39 +0200

    chore: dashboard cards

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { LeftSidebar } from "../../components/dashboard/Sidebar";
 import { Layout } from "../../components/dashboard/Layout";
+import { DashboardStatsCard } from "../../components/dashboard/Stats";
+import { BugAntIcon } from "@heroicons/react/24/outline";
+
+const testStats = [
+  { title: "Crash Reports", formattedValue: "1,234", icon: <BugAntIcon />, description: "↗︎ 12% from last week" },
+  { title: "Critical Issues", formattedValue: "12", icon: <BugAntIcon />, description: "↗︎ 12% from last week" },
+  { title: "Users Affected", formattedValue: "1,234", icon: <BugAntIcon />, description: "↙ 12% from last week" },
+  { title: "New Issues", formattedValue: "1,234", icon: <BugAntIcon />, description: "↙ 12% from last week" }
+]
 
 export const Dashboard: React.FC = () => {
   return (
     <Layout>
-      <p>Test</p>
+      <div className="grid lg:grid-cols-4 mt-2 md:grid-cols-2 grid-cols-1 gap-6">
+        {
+          testStats.map((stat, i) => (
+            <DashboardStatsCard
+              key={i}
+              title={stat.title}
+              formattedValue={stat.formattedValue}
+              icon={stat.icon}
+              description={stat.description}
+            />
+          ))
+        }
+      </div>
     </Layout>
   );
 };
