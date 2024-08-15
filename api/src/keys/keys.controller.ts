6929bfab1686911d73c7fb90b24147834fbd0730
style77 <stylek777@gmail.com>
2024-08-15 20:19:23 +0200

    chore: verify key

--- api/src/keys/keys.controller.ts
+++ api/src/keys/keys.controller.ts
@@ -1,37 +1,70 @@
-import { Body, Controller, Get, NotFoundException, Patch, Post, Query, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Patch,
+  Post,
+  Query,
+  Req,
+  Headers,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { KeysService } from './keys.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateKeyDto, UpdateKeyDto } from './dto/key.dto';
 
 @Controller('keys')
 export class KeysController {
-    constructor(private keysService: KeysService) { }
+  constructor(private keysService: KeysService) {}
 
-    @UseGuards(JwtAuthGuard)
-    @Get('/')
-    list(@Req() req,
-        @Query('page') page: number = 1,
-        @Query('limit') limit: number = 10,
-        @Query('sort') sort: string = 'id',
-        @Query('order') order: 'ASC' | 'DESC' = 'ASC',
-        @Query('filter') filter?: string) {
-        const user: any = req.user;
-        return this.keysService.findAllUserKeys(user, { page, limit, sort, order, filter });
-    }
+  @UseGuards(JwtAuthGuard)
+  @Get('/')
+  list(
+    @Req() req,
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+    @Query('sort') sort: string = 'id',
+    @Query('order') order: 'ASC' | 'DESC' = 'ASC',
+    @Query('filter') filter?: string,
+  ) {
+    const user: any = req.user;
+    return this.keysService.findAllUserKeys(user, {
+      page,
+      limit,
+      sort,
+      order,
+      filter,
+    });
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Post('/')
-    createKey(@Req() req, @Body() createKeyDto: CreateKeyDto) {
-        return this.keysService.createKey(req.user, createKeyDto.allowed_domains);
-    }
+  @UseGuards(JwtAuthGuard)
+  @Post('/')
+  createKey(@Req() req, @Body() createKeyDto: CreateKeyDto) {
+    return this.keysService.createKey(req.user, createKeyDto.allowed_domains);
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Patch('/')
-    async updateKey(@Req() req, @Body() updateKeyDto: UpdateKeyDto) {
-        const keyObj = await this.keysService.findKey(updateKeyDto.key);
-        if (!keyObj || keyObj.user.id !== req.user.id) {
-            throw new NotFoundException('Key not found');
-        }
-        return this.keysService.updateKey(keyObj.key, updateKeyDto.allowed_domains);
+  @UseGuards(JwtAuthGuard)
+  @Patch('/')
+  async updateKey(@Req() req, @Body() updateKeyDto: UpdateKeyDto) {
+    const keyObj = await this.keysService.findKey(updateKeyDto.key);
+    if (!keyObj || keyObj.user.id !== req.user.id) {
+      throw new NotFoundException('Key not found');
     }
+    return this.keysService.updateKey(keyObj.key, updateKeyDto.allowed_domains);
+  }
+
+  @Get('/verify')
+  async verify(@Req() req: Request, @Headers('api-key') apiKey: string) {
+    const referer = req.headers.referer || req.headers.origin;
+
+    const result = await this.keysService.verifyKey(apiKey, referer);
+    if (!result) {
+      throw new NotFoundException('Domain not allowed.');
+    }
+    return {
+      success: true,
+    };
+  }
 }
--- api/src/keys/keys.service.ts
+++ api/src/keys/keys.service.ts
@@ -5,60 +5,76 @@ import { Like, Repository } from 'typeorm';
 import { User } from 'src/users/entities/user.entity';
 
 interface QueryParams {
-    page: number;
-    limit: number;
-    sort: string;
-    order: 'ASC' | 'DESC';
-    filter?: string;
-  }
+  page: number;
+  limit: number;
+  sort: string;
+  order: 'ASC' | 'DESC';
+  filter?: string;
+}
 
 @Injectable()
 export class KeysService {
-    constructor(
-        @InjectRepository(Key)
-        private keysRepository: Repository<Key>,
-    ) {}
+  constructor(
+    @InjectRepository(Key)
+    private keysRepository: Repository<Key>,
+  ) {}
 
-    private static generateKey(): string {
-        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  private static generateKey(): string {
+    return (
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15)
+    );
+  }
+
+  async createKey(user: User, allowed_domains: string[]): Promise<Key> {
+    const key = this.keysRepository.create({
+      key: KeysService.generateKey(),
+      user: user,
+      allowed_domains: allowed_domains,
+    });
+    return this.keysRepository.save(key);
+  }
+
+  async findKey(key: string): Promise<Key> {
+    return this.keysRepository.findOne({ where: { key } });
+  }
+
+  async findAllUserKeys(user: User, params: QueryParams): Promise<Key[]> {
+    const { page, limit, sort, order, filter } = params;
+    const where: any = { user };
+
+    if (filter) {
+      where.keyName = Like(`%${filter}%`);
     }
 
-    async createKey(user: User, allowed_domains: string[]): Promise<Key> {
-        const key = this.keysRepository.create({
-            key: KeysService.generateKey(),
-            user: user,
-            allowed_domains: allowed_domains,
-        });
-        return this.keysRepository.save(key);
+    return this.keysRepository.find({
+      where,
+      order: { [sort]: order },
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+  }
+
+  async deleteKey(key: string): Promise<void> {
+    await this.keysRepository.delete({ key });
+  }
+
+  async updateKey(key: string, allowed_domains: string[]): Promise<Key> {
+    const keyToUpdate = await this.findKey(key);
+    keyToUpdate.allowed_domains = allowed_domains;
+    return this.keysRepository.save(keyToUpdate);
+  }
+
+  async verifyKey(key: string, referer: string): Promise<boolean> {
+    const keyObj = await this.findKey(key);
+    if (!keyObj) {
+      return false;
     }
 
-    async findKey(key: string): Promise<Key> {
-        return this.keysRepository.findOne({ where: { key } });
+    if (!keyObj.allowed_domains.includes(referer)) {
+      return false;
     }
 
-    async findAllUserKeys(user: User, params: QueryParams): Promise<Key[]> {
-        const { page, limit, sort, order, filter } = params;
-        const where: any = { user };
-    
-        if (filter) {
-          where.keyName = Like(`%${filter}%`);
-        }
-    
-        return this.keysRepository.find({
-          where,
-          order: { [sort]: order },
-          skip: (page - 1) * limit,
-          take: limit,
-        });
-      }
-
-    async deleteKey(key: string): Promise<void> {
-        await this.keysRepository.delete({ key });
-    }
-
-    async updateKey(key: string, allowed_domains: string[]): Promise<Key> {
-        const keyToUpdate = await this.findKey(key);
-        keyToUpdate.allowed_domains = allowed_domains;
-        return this.keysRepository.save(keyToUpdate);
-    }
+    return true;
+  }
 }
