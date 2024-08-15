bf92f0b57d2707754f7250e6b1803df8399e5838
style77 <stylek777@gmail.com>
2024-08-15 23:14:28 +0200

    chore: handle anonymous user, session and errors

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,20 +1,32 @@
 import type { IConfig } from "./config";
 import defaultConfig from "./config";
-import InteractionLogger from "./loggers/interaction";
-import MouseMovementLogger from "./loggers/mouse_movements";
+import InteractionLogger, { UserInteraction } from "./loggers/interaction";
+import MouseMovementLogger, { MouseMovement } from "./loggers/mouse_movements";
 import InteractionTracker from "./trackers/interaction";
 import MouseMovementTracker from "./trackers/mouse_movement";
+
+type ErrorEvent = {
+    isTrusted: boolean;
+    message: string;
+    filename: string;
+    lineno: number;
+    colno: number;
+    error: Error;
+    timestamp: number;
+}
 
 interface IWhatHappened {
     publicKey: string;
     config: IConfig;
 
-    onErrorOccurred(): void;
+    onErrorOccurred(errorEvent: ErrorEvent): void;
     start(): void;
 }
 
 class WhatHappened implements IWhatHappened {
     publicKey: string;
+    private sessionID: string = this.generateSessionID();
+    private sessionStartedAt: number = Date.now();
     private mouseTracker: MouseMovementTracker;
     private interactionTracker: InteractionTracker;
     private mouseLogger: MouseMovementLogger;
@@ -67,23 +79,93 @@ class WhatHappened implements IWhatHappened {
         this.interactionTracker.trackInteractions();
 
         if (easySetup) {
-            window.addEventListener('error', this.onErrorOccurred.bind(this));
+            window.addEventListener('error', (errorEvent) => this.onErrorOccurred({
+                isTrusted: errorEvent.isTrusted,
+                message: errorEvent.message,
+                filename: errorEvent.filename,
+                lineno: errorEvent.lineno,
+                colno: errorEvent.colno,
+                error: errorEvent.error,
+                timestamp: Date.now(),
+            }));
         }
     }
 
-    onErrorOccurred(): void {
+    onErrorOccurred(errorEvent: ErrorEvent): void {
         const mouseMovements = this.mouseLogger.getMovements();
         const interactions = this.interactionLogger.getInteractions();
 
         if (mouseMovements.length > 0 || interactions.length > 0) {
-            this.sendDataToServer({ mouseMovements, interactions });
+            this.sendDataToServer({ mouseMovements, interactions, error: errorEvent });
             this.mouseLogger.clearMovements();
             this.interactionLogger.clearInteractions();
         }
     }
 
-    private sendDataToServer(data: any): void {
-        console.log(data);
+    private generateSessionID(): string {
+        return Math.random().toString(36).substr(2, 9);
+    }
+
+    private getUserID(): string {
+        let userID = localStorage.getItem('whathappened-user-id');
+        if (!userID) {
+            userID = Math.random().toString(36).substr(2, 9);
+            localStorage.setItem('whathappened-user-id', userID);
+        }
+
+        return userID;
+    }
+
+    private generateErrorId(errorEvent: ErrorEvent): string {
+        const errorString = `${errorEvent.message}-${errorEvent.filename}-${errorEvent.lineno}-${errorEvent.colno}-${errorEvent.timestamp}`;
+        return btoa(errorString);
+    }
+
+    private sendDataToServer(data: {
+        mouseMovements: MouseMovement[],
+        interactions: UserInteraction[],
+        error: ErrorEvent,
+    }): void {
+        const userID = this.getUserID();
+        const errorID = this.generateErrorId(data.error);
+        const sessionID = this.sessionID;
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': this.publicKey
+        };
+
+        const body = {
+            error: {
+                id: errorID,
+                message: data.error.message,
+                filename: data.error.filename,
+                lineno: data.error.lineno,
+                colno: data.error.colno,
+                error: data.error.error,
+                timestamp: data.error.timestamp,
+            },
+            session: {
+                id: sessionID,
+                user: userID,
+                ua: navigator.userAgent,
+                url: window.location.href,
+                referrer: document.referrer,
+                screen: {
+                    width: window.screen.width,
+                    height: window.screen.height,
+                },
+                viewport: {
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                },
+                time: {
+                    startedAt: this.sessionStartedAt,
+                }
+            },
+            mouseMovements: data.mouseMovements,
+            interactions: data.interactions,
+        };
     }
 }
 
