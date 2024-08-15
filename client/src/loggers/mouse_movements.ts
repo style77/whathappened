26294c5e1a8ea0addcdbfef4098a690522f730ac
style77 <stylek777@gmail.com>
2024-08-15 21:29:11 +0200

    fix(client): use type in mouse movements

--- client/src/loggers/mouse_movements.ts
+++ client/src/loggers/mouse_movements.ts
@@ -3,13 +3,13 @@ import type Observer from "../trackers/observer";
 export type MouseMovement = { x: number, y: number, timestamp: number };
 
 class MouseMovementLogger implements Observer<MouseMovement> {
-    private movements: { x: number, y: number }[] = [];
+    private movements: MouseMovement[] = [];
 
     update(data: MouseMovement): void {
         this.movements.push(data);
     }
 
-    getMovements(): { x: number, y: number }[] {
+    getMovements(): MouseMovement[] {
         return this.movements;
     }
 
