d8dea4725a68de011126d56aba341826e5cb980d
style77 <stylek777@gmail.com>
2024-08-17 16:50:08 +0200

    chore(api): error entity

---
+++ api/src/report/entities/error.entity.ts
@@ -0,0 +1,55 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  Unique,
+  OneToMany,
+  BeforeInsert,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { SessionError } from './session-error.entity';
+import { Key } from 'src/keys/entities/key.entity';
+
+@Entity('errors')
+@Unique(['message', 'filename', 'lineno', 'colno'])
+export class Error {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  message: string;
+
+  @Column()
+  filename: string;
+
+  @Column()
+  lineno: number;
+
+  @Column()
+  colno: number;
+
+  @Column('text')
+  errorStack: string;
+
+  @CreateDateColumn()
+  firstOccurredAt: Date;
+
+  @UpdateDateColumn()
+  lastOccurredAt: Date;
+
+  @BeforeInsert()
+  generateErrorId() {
+    const errorString = `${this.message}-${this.filename}-${this.lineno}-${this.colno}-${Date.now()}`;
+    this.id = Buffer.from(errorString).toString('base64');
+  }
+
+  @OneToMany(() => SessionError, (sessionError) => sessionError.error)
+  sessionErrors: SessionError[];
+
+  @ManyToOne(() => Key, { nullable: true })
+  @JoinColumn({ name: 'key_id' })
+  key: Key;
+}
