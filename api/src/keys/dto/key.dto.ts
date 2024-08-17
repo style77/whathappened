9ee7bd041019c4d10d90eea20e56755d53ce87fa
style77 <stylek777@gmail.com>
2024-08-17 23:58:14 +0200

    fix(api): key dto should not require protocol

--- api/src/keys/dto/key.dto.ts
+++ api/src/keys/dto/key.dto.ts
@@ -1,17 +1,17 @@
-import { IsNotEmpty, IsArray, IsUrl } from 'class-validator'
+import { IsNotEmpty, IsArray, IsUrl } from 'class-validator';
 
 export class CreateKeyDto {
-    @IsNotEmpty()
-    @IsArray()
-    @IsUrl({}, { each: true })
-    allowed_domains: string[];
+  @IsNotEmpty()
+  @IsArray()
+  @IsUrl({ require_protocol: false }, { each: true })
+  allowed_domains: string[];
 }
 
 export class UpdateKeyDto {
-    @IsNotEmpty()
-    key: string;
+  @IsNotEmpty()
+  key: string;
 
-    @IsArray()
-    @IsUrl({}, { each: true })
-    allowed_domains: string[];
+  @IsArray()
+  @IsUrl({ require_protocol: false }, { each: true })
+  allowed_domains: string[];
 }
