38207c659d7122fc8d27f54dd6d596090f24dea1
style77 <stylek777@gmail.com>
2024-08-17 14:29:15 +0200

    chore(client): trim old records

--- client/src/loggers/mouse_movements.ts
+++ client/src/loggers/mouse_movements.ts
@@ -4,12 +4,20 @@ export type MouseMovement = { x: number, y: number, timestamp: number };
 
 class MouseMovementLogger implements Observer<MouseMovement> {
     private movements: MouseMovement[] = [];
+    private interval: number = 15000; // 15 seconds
 
     update(data: MouseMovement): void {
         this.movements.push(data);
+        this.trimOldRecords();
+    }
+
+    private trimOldRecords(): void {
+        const now = Date.now();
+        this.movements = this.movements.filter(movement => (now - movement.timestamp) <= this.interval);
     }
 
     getMovements(): MouseMovement[] {
+        this.trimOldRecords();
         return this.movements;
     }
 
